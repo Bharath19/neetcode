d56efadcb7f4c4b5ac3846a95f8794e44af3f966
Bharath <bharathganesan17@gmail.com>
2026-04-18 22:19:00 +0200

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute force
+        # for first_idx in range(len(nums)-1):
+        #     for second_idx in range(first_idx+1, len(nums)):
+        #         if nums[first_idx] + nums[second_idx] == target:
+        #             return [first_idx, second_idx]
+
+        lookup = {}
+        for idx, value in enumerate(nums):
+            lookup[value] = idx
+        
+        for idx, value in enumerate(nums):
+            if target - value in lookup:
+                if idx == lookup[target-value]:
+                    continue
+                return [idx, lookup[target-value]]
